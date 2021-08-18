cd3584277c7a724681f0bb8686fb20ecb64bb195
leduynhat <nhat.cobb@gmail.com>
2021-08-18 12:57:03 +0700

    fix port

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
-// const express = require('express')
-// const app = express()
-// const path = require('path')
-
-// app.use('/public', express.static(path.join(__dirname, '/public')))
-
-// app.get('/', function(req, res){
-//     var cv = path.join(__dirname, '/public/index.html')
-//     res.sendFile(cv)
-// })
-
-// app.listen(3000, function(){
-    
-// })
-// var http = require('http');
-// var fs = require('fs');
-
-// var server = http.createServer(function(req, res){
-//     console.log('Request was made: ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     var myReadStream = fs.createReadStream(__dirname + '/public/index.html', 'utf-8');
-//     myReadStream.pipe(res);
-// })
-
-// server.listen(3000, '127.0.0.1');
-// console.log("oke let's get started!!!")
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const { process } = require("ipaddr.js");
+// const { process } = require("ipaddr.js");
 const app = express();
+const PORT = process.env.PORT || 3000
 
-app.listen(process.env.PORT, () => {
-  console.log("Application started and Listening on port 3000");
+app.listen(PORT, () => {
+  console.log("Application started and Listening on port: " + PORT);
 });
 
 // server css as static
